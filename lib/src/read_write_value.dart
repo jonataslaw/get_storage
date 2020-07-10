745aba14076c22d972bde4bfbb53ef25ee50397e
Alif Akbar <alif.akbar@bukalapak.com>
2020-07-10 10:08:03 +0800

    add object encoder

--- lib/src/read_write_value.dart
+++ lib/src/read_write_value.dart
@@ -6,11 +6,17 @@ class ReadWriteValue<T> {
   final String key;
   final T defaultValue;
   final StorageFactory getBox;
+  final EncodeObject objectEncoder;
 
-  ReadWriteValue(this.key, this.defaultValue, this.getBox);
+  ReadWriteValue(
+    this.key,
+    this.defaultValue,
+    this.getBox, [
+    this.objectEncoder,
+  ]);
 
   T get val => getBox().read(key) ?? defaultValue;
-  set val(T newValue) => getBox().write(key, newValue);
+  set val(T newValue) => getBox().write(key, newValue, objectEncoder);
 }
 
 extension Data<T> on T {
