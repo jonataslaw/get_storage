27d8ec73b5791e1ed01e9cbd99d1cdd2919c2ac9
yutae <dev.yutae@gmail.com>
2022-03-16 23:29:06 +0900

    Fix type cast error

--- lib/src/storage/html.dart
+++ lib/src/storage/html.dart
@@ -81,7 +81,7 @@ class StorageImpl {
       },
     );
     if (dataFromLocal != null) {
-      subject.value = json.decode(dataFromLocal.value) as Map<String, dynamic>?;
+      subject.value = json.decode(dataFromLocal.value) as Map<String, dynamic>;
     } else {
       await _writeToStorage(<String, dynamic>{});
     }
